c4d535fe005a1b73216f82d8f23c1415fe34fb5d
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-06-19 11:51:37 +0300

    Update cream.py

--- cream.py
+++ cream.py
@@ -6,3 +6,14 @@ def cream(cream_fatness, milk_fatness, creamfat_target, cream_target_mass):
   milk_share_mass = cream_target_mass / coeff
   return [coeff, cream_share_mass, milk_share_mass]
 
+cream_fatness = st.slider("Исходное значение жирности сливок", 35, 54, 45, 1)
+milk_fatness = st.slider(f"Жирность молока, которым разбавляют сливки", 0, 5, 3.2, 0.2)
+creamfat_target = st.slider(f"Целевое значение жирности сливок", 35, 54, 45, 1)
+cream_target_mass = st.number_input(f"Сколько сливок целевой жирности нужно получить, кг", value=0)
+
+cream_output = cream(cream_fatness, milk_fatness, creamfat_target, cream_target_mass)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Коэффициент пропорции (сливки/молоко)", cream_output[0], "")
+col2.metric("Сколько сливок лить для разбавления", cream_output[1], "кг")
+col3.metric("Сколько молока нужно лить для разбавления", cream_output[2], "кг")

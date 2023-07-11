b5ed5102dd4139b56998ec430d87b95374021a5e
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-07-11 19:17:10 +0300

    Update cream.py

--- cream.py
+++ cream.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 def cream(cream_fatness, milk_fatness, creamfat_target, cream_target_mass):
-  coeff = (creamfat_target - milk_fatness) / (cream_fatness - creamfat_target)
-  cream_share_mass = (cream_target_mass * coeff) / (1 + coeff)
-  milk_share_mass = cream_share_mass / coeff
-  return [coeff, cream_share_mass, milk_share_mass]
+  milk_share_mass = cream_target_mass - ((0.85 * cream_target_mass * creamfat_target) / (0.15 * cream_fatness * milk_fatness))
+  cream_share_mass = (cream_target_mass - milk_share_mass * 1.15) / 1.15
+  sugar_share_mass = (milk_share_mass + cream_share_mass) * 0.15
+  return [cream_share_mass, milk_share_mass, sugar_share_mass]
 st.markdown('''<a href="http://kaizen-consult.ru/"><img src='https://www.kaizen.com/images/kaizen_logo.png' style="width: 50%; margin-left: 25%; margin-right: 25%; text-align: center;"></a><p>''', unsafe_allow_html=True)
 st.markdown('''<h1>Приложение для опеределния жирности сливок</h1>''', unsafe_allow_html=True)
 st.markdown('''<h2>Введите данные:</h2>''', unsafe_allow_html=True)
@@ -16,7 +16,8 @@ cream_target_mass = st.number_input(f"Сколько сливок целевой
 cream_output = cream(cream_fatness, milk_fatness, creamfat_target, cream_target_mass)
 st.markdown('''<h2> </h2>''', unsafe_allow_html=True)
 st.markdown('''<h2>Результаты расчетов:</h2>''', unsafe_allow_html=True)
-st.metric("Коэффициент пропорции (сливки/молоко)", f'{round(cream_output[0], 2)}')
-st.metric("Сколько сливок лить для разбавления", f'{round(cream_output[1], 2)} КГ')
-st.metric("Сколько молока нужно лить для разбавления", f'{round(cream_output[2], 2)} КГ')
+st.metric("Сколько лить сливок", f'{round(cream_output[0], 2)} КГ')
+st.metric("Сколько лить молока", f'{round(cream_output[1], 2)} КГ')
+st.metric("Сколько сыпать сахара", f'{round(cream_output[2], 2)} КГ')
 #st.metric("Проверка", round(cream_output[1], 2) + round(cream_output[2], 2))
+

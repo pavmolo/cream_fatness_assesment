751636213e65e02f0e6cd952ee1d74dda55f152a
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-07-12 09:34:33 +0300

    Update cream.py

--- cream.py
+++ cream.py
@@ -18,10 +18,8 @@ cream_fatness = st.slider("Исходное значение жирности с
 milk_fatness = st.slider("Жирность молока, которым разбавляют сливки, %", 0.0, 5.0, 3.2, 0.2)
 creamfat_target = st.slider("Целевое значение жирности сливок, %", 35, 54, 45, 1)
 cream_target_mass = st.number_input(f"Сколько сливок целевой жирности нужно получить, кг", value=200)
-sugar_mode = st.radio(
-    "Укажите, какой способ добавления слабости вы используете",
-    ('Сахар', 'Сгущенка (8%)'))
-if genre == 'Сахар':
+sugar_mode = st.radio("Укажите, какой способ добавления слабости вы используете", ('Сахар', 'Сгущенка (8%)'))
+if sugar_mode == 'Сахар':
     cream_output = cream(cream_fatness, milk_fatness, creamfat_target, cream_target_mass)
     st.markdown('''<h2> </h2>''', unsafe_allow_html=True)
     st.markdown('''<h2>Результаты расчетов:</h2>''', unsafe_allow_html=True)

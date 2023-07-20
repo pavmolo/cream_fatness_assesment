2cdef340229bfdb501f106c9efe05779f8e4bb06
pavmolo <74321783+pavmolo@users.noreply.github.com>
2023-07-20 09:26:12 +0300

    Update cream.py

--- cream.py
+++ cream.py
@@ -20,12 +20,15 @@ creamfat_target = st.slider("Целевое значение жирности с
 cream_target_mass = st.number_input(f"Сколько сливок целевой жирности нужно получить, кг", value=200)
 sugar_mode = st.radio("Укажите, какой способ добавления слабости вы используете", ('Сахар', 'Сгущенка (8%)'))
 if sugar_mode == 'Сахар':
+  if cream_output[1] > 0:
     cream_output = cream(cream_fatness, milk_fatness, creamfat_target, cream_target_mass)
     st.markdown('''<h2> </h2>''', unsafe_allow_html=True)
     st.markdown('''<h2>Результаты расчетов:</h2>''', unsafe_allow_html=True)
     st.metric("Сколько лить сливок", f'{round(cream_output[0], 2)} КГ')
     st.metric("Сколько лить молока", f'{round(cream_output[1], 2)} КГ')
     st.metric("Сколько сыпать сахара", f'{round(cream_output[2], 2)} КГ')
+  else:
+    st.header(f'Если вы добавите к сливкам {cream_fatness}% жирности сахар в размере 150 г на 1 кг, то получите сироп жирностью {cream_fatness/ 130}%, что ниже целевой жирности {creamfat_target}%')
 else:
     cream_output_cm = condenced_milk(cream_fatness, milk_fatness, creamfat_target, cream_target_mass)
     st.markdown('''<h2> </h2>''', unsafe_allow_html=True)
